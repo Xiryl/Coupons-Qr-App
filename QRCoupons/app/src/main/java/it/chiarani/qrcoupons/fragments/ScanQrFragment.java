056783ab61e26894f4c7201f5a52c2ccf16feb54
Fabio Chiarani <fabio@chiarani.it>
2018-07-02 15:47:21 +0200

    add tips helper

--- QRCoupons/app/src/main/java/it/chiarani/qrcoupons/fragments/ScanQrFragment.java
+++ QRCoupons/app/src/main/java/it/chiarani/qrcoupons/fragments/ScanQrFragment.java
@@ -5,8 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import it.chiarani.qrcoupons.R;
+import it.chiarani.qrcoupons.helpers.Tips;
+import uk.co.samuelwall.materialtaptargetprompt.MaterialTapTargetPrompt;
+import uk.co.samuelwall.materialtaptargetprompt.extras.backgrounds.RectanglePromptBackground;
 
 public class ScanQrFragment extends Fragment {
 
@@ -23,6 +27,8 @@ public class ScanQrFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View rootView =  inflater.inflate(R.layout.qrscan_fragment_layout, container, false);
+
+    Tips.MakeTipsForQrScanFragment(rootView, this);
 
     return rootView;
   }
---
+++ QRCoupons/app/src/main/java/it/chiarani/qrcoupons/helpers/Tips.java
@@ -0,0 +1,44 @@
+package it.chiarani.qrcoupons.helpers;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.view.View;
+
+import it.chiarani.qrcoupons.R;
+import it.chiarani.qrcoupons.fragments.ScanQrFragment;
+import uk.co.samuelwall.materialtaptargetprompt.MaterialTapTargetPrompt;
+
+public class Tips {
+  public Tips() {
+
+  }
+
+  public static void MakeTipsForQrScanFragment(View rootView, ScanQrFragment fragment) {
+
+    final MaterialTapTargetPrompt test = new MaterialTapTargetPrompt.Builder(fragment)
+        .setTarget(rootView.findViewById(R.id.qrscan_fragment_btn_valida))
+        .setPrimaryText("Non riesci a scannerizzarlo?")
+        .setSecondaryText("Inseriscilo manualmente da qua!").create();
+
+    final MaterialTapTargetPrompt test1 = new MaterialTapTargetPrompt.Builder(fragment)
+        .setTarget(rootView.findViewById(R.id.qrscan_fragment_btn_scannerizzaqr))
+        .setPrimaryText("Scannerizza il QR")
+        .setSecondaryText("Il tuo copupon verrà aggiunto automaticamente!")
+        .setPromptStateChangeListener(new MaterialTapTargetPrompt.PromptStateChangeListener()
+        {
+          @Override
+          public void onPromptStateChanged(MaterialTapTargetPrompt prompt, int state)
+          {
+            if (
+                state == MaterialTapTargetPrompt.STATE_DISMISSING        ||
+                state == MaterialTapTargetPrompt.STATE_NON_FOCAL_PRESSED ||
+                state == MaterialTapTargetPrompt.STATE_FOCAL_PRESSED
+                ) {
+              test.show();
+            }
+
+          }
+        }).show();
+  }
+}
