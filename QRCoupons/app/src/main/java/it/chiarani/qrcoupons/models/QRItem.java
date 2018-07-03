590bdd83560a664c6ff237fd9407413eb1960934
Fabio Chiarani <fabio@chiarani.it>
2018-07-03 10:38:08 +0200

    update qritem constructor

--- QRCoupons/app/src/main/java/it/chiarani/qrcoupons/models/QRItem.java
+++ QRCoupons/app/src/main/java/it/chiarani/qrcoupons/models/QRItem.java
@@ -5,22 +5,30 @@ import java.util.Date;
 public class QRItem {
 
   // private fileds
-  private int idItem;
+  private int    idItem;
   private String name;
   private String description;
-  private Date createdDate;
-  private Date expirationDate;
+  private Date   createdDate;
+  private Date   expirationDate;
   private String code;
   private String uri;
 
 
-  // constructor
+  // empty constructor
   public QRItem () {
 
   }
 
-  // add new item
-  public boolean addQR() {
-    return true;
+  public QRItem(int idItem, String name, String description, Date createdDate, Date expirationDate, String code, String uri) {
+    this.idItem         = idItem;
+    this.name           = name;
+    this.description    = description;
+    this.createdDate    = createdDate;
+    this.expirationDate = expirationDate;
+    this.code           = code;
+    this.uri            = uri;
   }
+
+
+
 }
